4e873694062aa9d413c3ff73029cc51dbdc80e15
lux-1n-tenebris <s.radosavljevic669@gmail.com>
2017-06-05 12:53:30 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function check(form) {
+	if (form.userdemo.value == "demo" && form.passdemo.value == "demo") {
+		window.open('target.html')
+	}else{
+	alert("Error Password or Username")
+	}
+}
+
+
+
+
